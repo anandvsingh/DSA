f7d23a3212b05cd05384b760289ab6b9ed0c21c4
anandvsingh <anand.iiitnr@gmail.com>
2024-01-15 22:15:52 +0530

    Insertion Sort start

---
+++ sorting/insertionSort/insertionSort/Program.cs
@@ -0,0 +1,29 @@
+﻿int min = 20;
+int max = 100;
+
+int[] input = new int[10];
+
+Random randnum = new Random(); 
+
+for (int i = 0; i<input.Length; i++)
+{
+    input[i]=randnum.Next(min, max);
+}
+
+
+for (int i = 1; i<input.Length; i++)
+{
+    int key = input[i];
+    int j=i-1;
+    while (j>=0 && input[j]>key)
+    {
+        input[j+1] = input[j];
+        j--;
+    }
+    input[j+1] = key;
+            
+}
+
+foreach(int a in input){
+    Console.WriteLine(a);
+}
